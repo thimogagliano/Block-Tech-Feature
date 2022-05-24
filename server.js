77a58f131a859f97eb8e56bd5863192715e1a42d
thimogagliano <94388546+thimogagliano@users.noreply.github.com>
2022-05-24 14:41:00 +0200

    Aanpassing server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express()
 //error handling
 app.use((err, req, res, next) => {
     console.error(err.stack)
-    res.status(500).send('Something broke!')
+    res.status(400).send('Something broke!')
   })
 
 // pad naar de about pagina
